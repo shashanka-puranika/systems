72902358c72b7d1f67342c2baad6669919a32ec1
shashanka-puranika <shashankapuranika747@gmail.com>
2026-01-07 13:58:57 +0000

    Initial

--- limma.r
+++ limma.r
@@ -1,1 +1,17 @@
+
+
+
+
+
+library(limma)
+
+data("swirl")
+group <- factor(c(rep("A",3), rep("B",3)))
+design <- model.matrix(~group)
+
+fit <- lmFit(swirl, design)
+fit <- eBayes(fit)
+
+topTable(fit)
+
 library(maftools)
